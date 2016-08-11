12d26947a9efff9c8fbf96df4169906086c52cb9
Jimmy <jmessmer@live.unc.edu>
2016-08-10 22:20:57 -0400

    Still working on rps-graph generation

--- rgraph.py
+++ rgraph.py
@@ -1,4 +1,6 @@
 import random
+import copy
+import collections
 '''
 Define a class for a Rock-Paper-Scissors graph
 (A graph where each node has  n/2 out edges for n nodes and n/2 in edges)
@@ -9,14 +11,20 @@ class Edge:
         self.outnode = outnode
         self.weight = weight
 
+    def __str__(self):
+        return self.innode.name + " --> " + self.outnode.name
+
+    def __repr__(self):
+        return self.__str__()
+
 class Node:
-    outedges = []
-    inedges = []
     def __init__(self, name, degree):
         self.name = name
         self.degree = degree
+        self.outedges = []
+        self.inedges = []
     def __str__(self):
-        return "Node Name:\t" + self.name + "\r\nNode Degree:\t" + str(self.degree)
+        return "Node Name:\t" + self.name# + "\r\nNode Degree:\t" + str(self.degree)
     def __repr__(self):
         return self.__str__()
     def addoutedge(self, edge):
@@ -31,10 +39,12 @@ class Node:
         return False
 class Rgraph(object):    
     nodes = []
+    nodequeue = collections.deque()
+    outcompletenodes = []
     def gennodes(self, numnodes):
         for i in range(0, numnodes):
             self.nodes.append(Node(str(i), numnodes - 1))
-            print(self.nodes[i])
+#            print(self.nodes[i])
 
     def choosenextnode(self, startnode):
         next = random.randint(0, len(self.nodes) - 1)
@@ -43,23 +53,48 @@ class Rgraph(object):
         return next
     
     def genedges(self):
+        nodequeue = collections.deque()
         # Need to have n/2 out edges and n/2 in edges
         completednodes = []
         startnode = random.randint(0, len(self.nodes) - 1)
-        while len(completednodes) != len(self.nodes):
-            node = self.nodes[startnode]
-            next = self.choosenextnode(node)
-            nextnode = self.nodes[next]
-            node.addoutedge(Edge(node, nextnode))
-            if node.iscomplete() == True:
-                completednodes.append(node)
-            startnode = next
 
-    def print(self):
-        for node in nodes:
+        node = self.nodes[startnode]
+        self.nodequeue.append(node)
+        while self.nodequeue:
+            node = self.nodequeue.pop()
+            possiblenodes = copy.copy(self.nodes)
+            possiblenodes.remove(node)
             print(node)
-            print("\t" + node.outedges)
-            print("\t" + node.inedges)
+            self.outcompletenodes.append(node)
+            self.processnode(node, possiblenodes)
+        
+        #print(node)
+
+    def processnode(self, node, possiblenodes):
+        next = random.randint(0, len(possiblenodes) - 1)
+        nextnode1 = possiblenodes[next]
+        possiblenodes.remove(nextnode1)
+        next = random.randint(0, len(possiblenodes) - 1)
+        nextnode2 = possiblenodes[next]
+        possiblenodes.remove(nextnode2)
+        # While we're at it, add edges for nodes that beat our starting node
+        for n in possiblenodes:
+            n.addoutedge(Edge(n, node))
+            
+        if not(nextnode1 in self.outcompletenodes):
+            self.nodequeue.append(nextnode1)
+        if not(nextnode2 in self.outcompletenodes):
+            self.nodequeue.append(nextnode2)
+
+        node.addoutedge(Edge(node, nextnode1))
+        node.addoutedge(Edge(node, nextnode2))
+
+    
+    def print(self):
+        for node in self.nodes:
+            print(node)
+            print("\t" + str(node.outedges))
+            print("\t" + str(node.inedges))
             
             
 def main():
